c456231c3cf1b7e2561bcc137f709f1ded4ee3e6
Daire <122104361+DaireLuus@users.noreply.github.com>
2023-01-11 11:18:59 +0200

    Delete module-daire-study-unit5repo directory

--- module-daire-study-unit5repo/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © Lumav Commerce OÜ. All rights reserved.
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Lumav_DaireStudyUnit5repo',
-    __DIR__
-);
