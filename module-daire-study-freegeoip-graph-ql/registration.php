8fef2e23b79a5df03c897d40905e1585e2345b69
Daire <122104361+DaireLuus@users.noreply.github.com>
2023-01-11 11:15:18 +0200

    Delete module-daire-study-freegeoip-graph-ql directory

--- module-daire-study-freegeoip-graph-ql/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © Lumav. All rights reserved.
- * See LICENSE.txt for license details.
- */
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Lumav_DaireStudyFreegeoipGraphQl', __DIR__);
